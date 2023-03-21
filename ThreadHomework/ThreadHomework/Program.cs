9de3dd76d6b1049cc5ba952e5d94e4ef486d49a4
NurayGurbanova <nuraygurbanova77@gmail.com>
2023-03-21 14:17:19 +0400

    Intro added

---
+++ ThreadHomework/ThreadHomework/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ThreadHomework
+{ 
+    internal class Program
+    {
+        /* 1 
+        static void func_start() 
+        { 
+            for (int i = 0; i < 10; i++) 
+            { 
+                Console.WriteLine("Function start"); 
+            } 
+        } 
+ 
+        static void func_out() 
+        { 
+            Thread th = new Thread(func_start); 
+            th.Start(); 
+            th.Join(); 
+            Console.WriteLine("Function finished"); 
+        } 
+ 
+        static void Main(string[] args) 
+        { 
+            func_out(); 
+        } 
+        */
+
+        //2 
+        static void func_start()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Function start");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread th = new Thread(func_start);
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    th.Start();
+                    th.Join();
+                    Console.WriteLine("Function finished");
+                    break;
+                case 2:
+                    Console.WriteLine("Thread is abort");
+                    break;
+                    th.Start();
+                    Console.WriteLine("Main aborting new thread.");
+                    th.Abort();
+            }
+        }
+    }
+}
