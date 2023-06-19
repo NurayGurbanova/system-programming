2e41b87673acb5650f20922b43975c4a9c9cc041
Nuray <104344313+NurayGurbanova@users.noreply.github.com>
2023-06-19 14:16:32 +0400

    HW

---
+++ 12.05.js
@@ -0,0 +1,117 @@
+//1
+// function comparison(a, b) 
+// {
+//     if (a < b) 
+//     {
+//       return -1;
+//     } 
+//     else if (a > b) 
+//     {
+//       return 1;
+//     } 
+//     else 
+//     {
+//       return 0;
+//     }
+//   }
+
+//   console.log(comparison(45,72));
+
+//2
+// function factorial(a) 
+// {
+//     if (a > 0) 
+//     {
+//       return a * factorial(a - 1);
+//     } 
+//     else {
+//         return 1;
+//     }
+//   }
+  
+//   console.log(factorial(10));
+
+//3
+// var number1 = prompt('Enter first number:');
+// var number2 = prompt('Enter second number:');
+// var number3 = prompt('Enter third number:');
+
+// function connect(num1,num2,num3)
+// {
+//     var result = Number(String(num1) + String(num2) + String(num3));
+//     return result;
+// }
+
+// console.log(connect(number1,number2,number3));
+
+//4
+// function S(leng, wid) 
+// {
+//     if (wid === undefined) 
+//     {
+//       return leng * leng;
+//     }
+//     else 
+//     {
+//       return leng * wid;
+//     }
+// }
+
+// console.log(S(3));
+
+
+//7
+// function time(h,m,s)
+// {
+//     if (m === undefined) 
+//     {
+//         m = "00";
+//     }
+    
+//     if (s === undefined) 
+//     {
+    //         s = "00";
+    //     }
+    //     var result = h + ":" + m + ":" + s;
+    //     return result;
+    // }
+    // var hour = 2;
+    // var min = undefined;
+    // var sec = undefined;
+    // console.log(time(hour,min,sec));
+    
+    //8
+    
+    // var hours = prompt('Enter hours: ');
+    // var minutes = prompt('Enter minutes:');
+// var seconds = prompt('Enter seconds:');
+
+// function Tosec(h,m,s)
+// {
+    //     var s_h = h * 3600;
+    //     var s_m = m * 60;
+    //     var result = s_h + s_m + s;
+    
+    //     return result;
+// }
+
+// console.log(Tosec(hours,minutes,seconds));
+
+//9
+// var seconds = prompt('Enter seconds:');
+
+// function Tonormaltime(s)
+// {
+//     if(s/3600)
+//     var h = s / 3600;
+//     var m = (s / 3600) % 60;
+//     var s = s % 60;
+//     var result = h + ":" + m +":"+ s;
+
+//     return result;
+// }
+
+// console.log(Tonormaltime(seconds));
+
+
+//5, 6, 10 - не знаю как сделать
