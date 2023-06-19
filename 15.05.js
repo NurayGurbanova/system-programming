3f5a2ac4f8d575100c0df40c912ccacb77b87a93
Nuray <104344313+NurayGurbanova@users.noreply.github.com>
2023-06-19 17:07:19 +0400

    hw

---
+++ 15.05.js
@@ -0,0 +1,36 @@
+//1
+
+// var car = {
+//     manufacturer: "",
+//     model: "",
+//     year: 0,
+//     averspeed: 0,
+  
+//     showinfo: function() 
+//     {
+//       console.log("manufacturer: " + this.manufacturer);
+//       console.log("model: " + this.model);
+//       console.log("year: " + this.year);
+//       console.log("average speed: " + this.averspeed + " km/h");
+//     },
+  
+//     time: function(S) 
+//     {
+//       var t = Math.floor(S / (4 * this.averspeed)) * 1;
+//       var t_s = S / this.averspeed + t;
+//       return t_s;
+//     }
+//   };
+  
+//   car.manufacturer = "Porsche";
+//   car.model = "911 R";
+//   car.year = 2016;
+//   car.averspeed = 100;
+  
+//   car.showinfo();
+  
+//   var S = 500; 
+//   var t = car.time(S);
+//   console.log("Time and distance: " + S + " km: " + t + " h");
+
+//2,3 не получается(((
